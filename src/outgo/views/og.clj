79a188227b893322517dd4b87dc2319eb68ee72c
Kevin Qin <defkevinend@gmail.com>
2013-10-15 21:10:04 +0800

    fixed issue of input string ""

--- src/outgo/views/og.clj
+++ src/outgo/views/og.clj
@@ -3,19 +3,21 @@
         [hiccup.form :only (form-to label text-field hidden-field text-area submit-button)])
   (:require [outgo.views.layout :as layout]))
 
-(defn get-value [username today-outgo field]
-  (if (= username "jessica") (field (first today-outgo)) (field (second today-outgo))))
+(defn get-value [username today-outgo field type]
+  (if (and (= type :int) (empty? today-outgo))
+   0
+    (if (= username "jessica") (field (first today-outgo)) (field (second today-outgo)))))
 
 (defn nested-form [usernames today-outgo]
   (let [fields [:breakfast :lunch :dinner :drink :individual :common]]
     (for [field fields]
       (html (label {:class "column label-part"} field field)
             (for [username usernames]
-              (text-field {:class "column one.column"} (str username "[" (name field) "]") (get-value username today-outgo field)))
+              (text-field {:class "column one.column"} (str username "[" (name field) "]") (get-value username today-outgo field :int)))
             [:div {:class "clear"}]
             (label {:class "column label-part"} "Note" "Note")
             (for [username usernames]
-              (text-area {:class "column one.column"} (str username "[" (name field) "_note" "]") (get-value username today-outgo (keyword (subs (str field "_note") 1)))))
+              (text-area {:class "column one.column"} (str username "[" (name field) "_note" "]") (get-value username today-outgo (keyword (subs (str field "_note") 1)) :nil)))
             [:div {:class "clear"}]))))
 
 (defn og-form [today-outgo]
